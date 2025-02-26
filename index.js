f8a26077e5597d99482dd6eaa57c4eda20f26364
Skizziedevs <kmblak8@gmail.com>
2025-02-26 04:53:44 -0800

    Fix CORS for Netlify frontend

--- index.js
+++ index.js
@@ -15,14 +15,11 @@ const QRCode = require("qrcode");
 
 
 const pool = new Pool({
-    user: 'postgres',
-
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL, // Use the correct database URL
+  ssl: {
+    rejectUnauthorized: false, // Required for NeonDB
+  },
 });
-
 
 
 app.use(cors({
