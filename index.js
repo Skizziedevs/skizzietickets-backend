dfe6fe1809cd16a4deea139203cdbe571c7b42d6
Skizziedevs <kmblak8@gmail.com>
2025-02-26 23:57:31 -0800

    enable localhosting

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const pool = new Pool({
 
 
 app.use(cors({
-  origin: ["https://campusly-ng.netlify.app", "http://localhost:5173/"],
+  origin: [ "http://localhost:5173"],
    credentials: true, // If using cookies or authentication
   methods: ["GET", "POST", "PUT", "DELETE"]
 }));
